1380f688e567f2cd55afe811ab8a6423e63f4042
lalatina11 <rahmadancandra@gmail.com>
2025-11-04 18:58:17 +0700

    feat: form schema for validating form

---
+++ src/lib/formSchema.ts
@@ -0,0 +1,46 @@
+import { z } from "zod";
+export const registerSchema = z.object({
+  name: z
+    .string()
+    .min(3, { message: "Panjang minimal 3 karakter" })
+    .max(128, { message: "Panjang maksimal 128 karakter" })
+    .regex(/^[a-zA-Z\s]+$/, {
+      message: "Nama hanya boleh berisi huruf dan spasi",
+    }),
+  username: z
+    .string()
+    .min(3, { message: "Panjang minimal 3 karakter" })
+    .max(32, { message: "Panjang maksimal 32 karakter" })
+    .regex(/^[a-z]+[a-z\-_]*[a-z]$/, {
+      message:
+        "Username hanya boleh berisi huruf kecil, - (dash), dan _ (underscore)",
+    }),
+  email: z.email({ message: "Format email tidak valid" }),
+  password: z
+    .string()
+    .min(8, { message: "Panjang minimal 8 karakter" })
+    .max(32, { message: "Panjang maksimal 32 karakter" })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+      message:
+        "Password harus berisi huruf kecil, huruf besar, angka, dan karakter spesiial",
+    }),
+  confirmPassword: z
+    .string()
+    .min(8, { message: "Panjang minimal 8 karakter" })
+    .max(32, { message: "Panjang maksimal 32 karakter" })
+    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/, {
+      message:
+        "Konfirmasi password harus berisi huruf kecil, huruf besar, angka, dan karakter spesiial",
+    }),
+  role: z.enum(["user", "merchant"]),
+});
+
+export type RegisterSchemaType = z.infer<typeof registerSchema>;
+
+export const loginSchema = registerSchema.omit({
+  name: true,
+  username: true,
+  confirmPassword: true,
+});
+
+export type LoginSchemaType = z.infer<typeof loginSchema>;
