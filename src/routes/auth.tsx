5cc59dc1df758cf465d0e2d0a789cef635e0a620
lalatina11 <rahmadancandra@gmail.com>
2025-11-04 18:59:02 +0700

    feat: create auth page for register and login

---
+++ src/routes/auth.tsx
@@ -0,0 +1,52 @@
+import LoginForm from "@/components/forms/LoginForm";
+import RegisterForm from "@/components/forms/RegisterForm";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { createFileRoute } from "@tanstack/react-router";
+import { useState } from "react";
+
+type Params = "login" | "register";
+
+export const Route = createFileRoute("/auth")({
+  component: RouteComponent,
+});
+
+function RouteComponent() {
+  const [params, setParams] = useState<Params>("login");
+  const isLoginPage = params === "login";
+  const switchParams = () => {
+    setParams(isLoginPage ? "register" : "login");
+  };
+  return (
+    <main className="flex justify-center items-center min-h-screen py-10">
+      <Card className="w-sm lg:w-md mx-auto">
+        <CardHeader>
+          <CardTitle>{isLoginPage ? "Login" : "Daftar"}</CardTitle>
+          <CardDescription>
+            {isLoginPage ? "Login ke akun anda" : "Daftarkan akun anda"}
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {isLoginPage ? <LoginForm /> : <RegisterForm />}
+        </CardContent>
+        <CardFooter>
+          <span className="flex gap-1 items-center text-xs cursor-pointer">
+            {isLoginPage ? "Belum punya akun?" : "Sudah punya akun?"}
+            <span
+              className="underline underline-offset-2"
+              onClick={switchParams}
+            >
+              {isLoginPage ? "daftar disini" : "login disini"}
+            </span>
+          </span>
+        </CardFooter>
+      </Card>
+    </main>
+  );
+}
