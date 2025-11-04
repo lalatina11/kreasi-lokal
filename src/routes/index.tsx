d831b1700809d7b8cb0a2ef72a8fef8921e7162d
lalatina11 <rahmadancandra@gmail.com>
2025-11-04 20:40:15 +0700

    feat: (temporary) landing page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,29 @@
 import { Button } from "@/components/ui/button";
+import { auth } from "@/lib/auth";
 import { createFileRoute } from "@tanstack/react-router";
+import { createServerFn } from "@tanstack/react-start";
+import { getRequest } from "@tanstack/react-start/server";
 
-export const Route = createFileRoute("/")({ component: App });
+const getUserSessionByServer = createServerFn({ method: "GET" }).handler(
+  async () => {
+    const { headers } = getRequest();
+    return await auth.api.getSession({ headers });
+  }
+);
+
+export const Route = createFileRoute("/")({
+  component: App,
+  ssr: true,
+  loader: async () => {
+    const session = await getUserSessionByServer();
+    return { session };
+  },
+});
 
 function App() {
+  const { session } = Route.useLoaderData();
+  console.log(session);
+
   return (
     <main className="min-h-screen">
       <Button>Test</Button>
