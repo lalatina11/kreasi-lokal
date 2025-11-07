f69c84033173968c114fdc159964e944defd6194
lalatina11 <rahmadancandra@gmail.com>
2025-11-07 12:30:17 +0700

    chore: homepage revamp

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,15 +1,7 @@
+import HomePageContainer from "@/components/containers/HomePageContainer";
 import { Button } from "@/components/ui/button";
-import { auth } from "@/lib/auth";
+import { getUserSessionByServer } from "@/server/renders/auth";
 import { createFileRoute } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/react-start";
-import { getRequest } from "@tanstack/react-start/server";
-
-const getUserSessionByServer = createServerFn({ method: "GET" }).handler(
-  async () => {
-    const { headers } = getRequest();
-    return await auth.api.getSession({ headers });
-  }
-);
 
 export const Route = createFileRoute("/")({
   component: App,
@@ -22,11 +14,12 @@ export const Route = createFileRoute("/")({
 
 function App() {
   const { session } = Route.useLoaderData();
-  console.log(session);
 
   return (
-    <main className="min-h-screen">
-      <Button>Test</Button>
-    </main>
+    <HomePageContainer>
+      <main className="min-h-screen">
+        <Button>Test</Button>
+      </main>
+    </HomePageContainer>
   );
 }
