f010972e10677b5274f0dc608e8a31d28ae6f32f
lalatina11 <rahmadancandra@gmail.com>
2025-12-01 22:10:24 +0700

    feat: json boolean switch helper

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,15 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+export function switchJSONBoolean(jsonBool: string) {
+  switch (jsonBool) {
+    case "true":
+      return true;
+    default:
+      return false;
+  }
 }
