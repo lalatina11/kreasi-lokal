16e4254192cbe9afd6d82dd77ea05feee9390dc2
lalatina11 <rahmadancandra@gmail.com>
2025-12-06 15:16:19 +0700

    feat: adding back button

--- src/routes/carts/index.tsx
+++ src/routes/carts/index.tsx
@@ -1,3 +1,5 @@
+import BackButton from "@/components/BackButton";
+import CartPageContainer from "@/components/containers/CartPageContainer";
 import CheckOutForm from "@/components/forms/CheckOutForm";
 import {
   Card,
@@ -34,7 +36,7 @@ export const Route = createFileRoute("/carts/")({
 });
 
 function RouteComponent() {
-  const { carts } = Route.useLoaderData();
+  const { carts, session } = Route.useLoaderData();
   const [selectedCartId, setselectedCartId] = useState(
     carts.length > 0 ? carts[0].id : ""
   );
@@ -47,79 +49,95 @@ function RouteComponent() {
     );
 
   return (
-    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3 pb-10">
-      <Card className="">
-        <CardHeader>
-          <CardTitle>Keranjang</CardTitle>
-          <CardDescription>jumlah keranjang: {carts.length}</CardDescription>
-          <CardContent className="flex flex-col gap-3">
-            {carts.map((cart, idx) => (
-              <Item
-                key={cart.id}
-                className={`flex flex-col ${cart.id === selectedCartId ? "cursor-default" : "cursor-pointer"}`}
-                onClick={() => {
-                  if (selectedCartId !== cart.id) {
-                    setselectedCartId(cart.id);
-                  }
-                }}
-                variant={selectedCartId !== cart.id ? "outline" : "muted"}
-              >
-                {/* <div> */}
-                {cart.items.map((item) => (
-                  <div
-                    key={item.id}
-                    className="flex gap-2 w-full items-center justify-between"
+    <CartPageContainer session={session}>
+      <main className="container mx-auto flex flex-col gap-2 p-3">
+        <BackButton />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 pb-10">
+          <Card className="">
+            <CardHeader>
+              <CardTitle>Keranjang</CardTitle>
+              <CardDescription>
+                jumlah keranjang: {carts.length}
+              </CardDescription>
+              <CardContent className="flex flex-col gap-3">
+                {carts.map((cart, idx) => (
+                  <Item
+                    key={cart.id}
+                    className={`flex flex-col ${cart.id === selectedCartId ? "cursor-default" : "cursor-pointer"}`}
+                    onClick={() => {
+                      if (selectedCartId !== cart.id) {
+                        setselectedCartId(cart.id);
+                      }
+                    }}
+                    variant={selectedCartId !== cart.id ? "outline" : "muted"}
                   >
-                    <span className="">{idx + 1}.</span>
-                    <span className="">
-                      {item.product.name.slice(0, 25)}
-                      {item.product.name.length > 25 && "..."}
-                    </span>
-                    <span className="text-xs">x{item.quantity}</span>
-                  </div>
-                ))}
-                {/* </div> */}
-              </Item>
-            ))}
-          </CardContent>
-        </CardHeader>
-      </Card>
-      {selectedCartId ? (
-        carts
-          .filter((cart) => cart.id === selectedCartId)
-          .map((cart) => (
-            <Card key={cart.id} className="md:flex col-span-1 lg:col-span-2">
-              <CardHeader>
-                <CardTitle>Detail Keranjang Terpilih</CardTitle>
-              </CardHeader>
-              <CardContent>
-                {cart.items.map((item) => (
-                  <div key={item.id} className="flex flex-col gap-1 text-sm">
-                    <span>Produk: {item.product.name}</span>
-                    <span>Quantity: {item.quantity}</span>
-                    <span>
-                      Harga Satuan: {switchCurrencyToIDR(item.product.price)}
-                    </span>
-                    <span>
-                      Total:{" "}
-                      {switchCurrencyToIDR(item.quantity * item.product.price)}
-                    </span>
-                    <div className="mt-2">
-                      <CheckOutForm
-                        totalPrice={item.quantity * item.product.price}
-                        cartId={selectedCartId}
-                      />
-                    </div>
-                  </div>
+                    {/* <div> */}
+                    {cart.items.map((item) => (
+                      <div
+                        key={item.id}
+                        className="flex gap-2 w-full items-center justify-between"
+                      >
+                        <span className="">{idx + 1}.</span>
+                        <span className="">
+                          {item.product.name.slice(0, 25)}
+                          {item.product.name.length > 25 && "..."}
+                        </span>
+                        <span className="text-xs">x{item.quantity}</span>
+                      </div>
+                    ))}
+                    {/* </div> */}
+                  </Item>
                 ))}
               </CardContent>
+            </CardHeader>
+          </Card>
+          {selectedCartId ? (
+            carts
+              .filter((cart) => cart.id === selectedCartId)
+              .map((cart) => (
+                <Card
+                  key={cart.id}
+                  className="md:flex col-span-1 lg:col-span-2"
+                >
+                  <CardHeader>
+                    <CardTitle>Detail Keranjang Terpilih</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {cart.items.map((item) => (
+                      <div
+                        key={item.id}
+                        className="flex flex-col gap-1 text-sm"
+                      >
+                        <span>Produk: {item.product.name}</span>
+                        <span>Quantity: {item.quantity}</span>
+                        <span>
+                          Harga Satuan:{" "}
+                          {switchCurrencyToIDR(item.product.price)}
+                        </span>
+                        <span>
+                          Total:{" "}
+                          {switchCurrencyToIDR(
+                            item.quantity * item.product.price
+                          )}
+                        </span>
+                        <div className="mt-2">
+                          <CheckOutForm
+                            totalPrice={item.quantity * item.product.price}
+                            cartId={selectedCartId}
+                          />
+                        </div>
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              ))
+          ) : (
+            <Card className="hidden md:flex col-span-1 lg:col-span-2">
+              <CardContent>Belum ada keranjang terpilih</CardContent>
             </Card>
-          ))
-      ) : (
-        <Card className="hidden md:flex col-span-1 lg:col-span-2">
-          <CardContent>Belum ada keranjang terpilih</CardContent>
-        </Card>
-      )}
-    </div>
+          )}
+        </div>
+      </main>
+    </CartPageContainer>
   );
 }
--- src/routes/orders/index.tsx
+++ src/routes/orders/index.tsx
@@ -1,3 +1,5 @@
+import BackButton from "@/components/BackButton";
+import CartPageContainer from "@/components/containers/CartPageContainer";
 import { Badge } from "@/components/ui/badge";
 import {
   Card,
@@ -43,12 +45,12 @@ export const Route = createFileRoute("/orders/")({
     const session = await getUserSessionByServer();
     if (!session) throw redirect({ to: "/auth" });
     const orders = await getOrders({ data: session.user.id });
-    return { orders };
+    return { orders, session };
   },
 });
 
 function RouteComponent() {
-  const { orders } = Route.useLoaderData();
+  const { orders, session } = Route.useLoaderData();
   const [selectedOrderId, setSelectedOrderId] = useState(
     orders.length > 0 ? orders[0].id : ""
   );
@@ -65,102 +67,120 @@ function RouteComponent() {
     status === "awaiting_shipment_number";
 
   return (
-    <div className="container mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 p-3 pb-10">
-      <Card className="">
-        <CardHeader>
-          <CardTitle>Transaksi</CardTitle>
-          <CardDescription>Jumlah Transaksi: {orders.length}</CardDescription>
-          <CardContent className="flex flex-col gap-3">
-            {orders.map((order, idx) => (
-              <Item
-                key={order.id}
-                className={`flex flex-col ${order.id === selectedOrderId ? "cursor-default" : "cursor-pointer"}`}
-                onClick={() => {
-                  if (selectedOrderId !== order.id) {
-                    setSelectedOrderId(order.id);
-                  }
-                }}
-                variant={selectedOrderId !== order.id ? "outline" : "muted"}
-              >
-                {order.items.map((item) => (
-                  <div
-                    key={item.id}
-                    className="flex gap-2 w-full items-center justify-between"
+    <CartPageContainer session={session}>
+      <main className="container mx-auto p-3 pb-10 flex flex-col gap-3">
+        <BackButton />
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3">
+          <Card className="">
+            <CardHeader>
+              <CardTitle>Transaksi</CardTitle>
+              <CardDescription>
+                Jumlah Transaksi: {orders.length}
+              </CardDescription>
+              <CardContent className="flex flex-col gap-3">
+                {orders.map((order, idx) => (
+                  <Item
+                    key={order.id}
+                    className={`flex flex-col ${order.id === selectedOrderId ? "cursor-default" : "cursor-pointer"}`}
+                    onClick={() => {
+                      if (selectedOrderId !== order.id) {
+                        setSelectedOrderId(order.id);
+                      }
+                    }}
+                    variant={selectedOrderId !== order.id ? "outline" : "muted"}
                   >
-                    <span className="">{idx + 1}.</span>
-                    <span className="">
-                      {item.product.name.slice(0, 25)}
-                      {item.product.name.length > 25 && "..."}
-                    </span>
-                    <span className="text-xs">x{item.quantity}</span>
-                  </div>
-                ))}
-              </Item>
-            ))}
-          </CardContent>
-        </CardHeader>
-      </Card>
-      {selectedOrderId ? (
-        orders
-          .filter((order) => order.id === selectedOrderId)
-          .map((order) => (
-            <Card key={order.id} className="md:flex col-span-1 lg:col-span-2">
-              <CardHeader>
-                <CardTitle>Detail Transaksi Terpilih</CardTitle>
-              </CardHeader>
-              <CardContent>
-                {order.items.map((item) => (
-                  <div key={item.id} className="flex flex-col gap-1 text-sm">
-                    <span>Produk: {item.product.name}</span>
-                    <span>Quantity: {item.quantity}</span>
-                    <span>
-                      Harga Satuan: {switchCurrencyToIDR(item.product.price)}
-                    </span>
-                    <span>
-                      Total:{" "}
-                      {switchCurrencyToIDR(item.quantity * item.product.price)}
-                    </span>
-                    <div className="flex gap-2 items-center mt-2">
-                      <span>Status:</span>
-                      <Badge
-                        variant={
-                          order.status === "completed" ? "default" : "secondary"
-                        }
+                    {order.items.map((item) => (
+                      <div
+                        key={item.id}
+                        className="flex gap-2 w-full items-center justify-between"
                       >
-                        {isOrderPending(order.status as string) ||
-                        isOrderAwaitingShipmentNumber(
-                          order.status as string
-                        ) ? (
-                          <Clock />
-                        ) : null}
-                        {switchOrderStatus(order.status as string)}
-                      </Badge>
-                    </div>
-                    <span className="text-xs text-muted-foreground">
-                      Catatan untuk penjual: {order.merchantNote}
-                    </span>
-                    {isOrderPending(order.status as string) && (
-                      <span className="text-sm">
-                        Penting: Harap hubungi{" "}
-                        {item.merchantAdditionalInfo.phoneNumber} untuk
-                        konfirmasi pembelian
-                      </span>
-                    )}
-                    {order.shippingNumber && (
-                      <span className="text-sm">
-                        Nomor Resi: {order.shippingNumber}
-                      </span>
-                    )}
-                  </div>
+                        <span className="">{idx + 1}.</span>
+                        <span className="">
+                          {item.product.name.slice(0, 25)}
+                          {item.product.name.length > 25 && "..."}
+                        </span>
+                        <span className="text-xs">x{item.quantity}</span>
+                      </div>
+                    ))}
+                  </Item>
                 ))}
               </CardContent>
+            </CardHeader>
+          </Card>
+          {selectedOrderId ? (
+            orders
+              .filter((order) => order.id === selectedOrderId)
+              .map((order) => (
+                <Card
+                  key={order.id}
+                  className="md:flex col-span-1 lg:col-span-2"
+                >
+                  <CardHeader>
+                    <CardTitle>Detail Transaksi Terpilih</CardTitle>
+                  </CardHeader>
+                  <CardContent>
+                    {order.items.map((item) => (
+                      <div
+                        key={item.id}
+                        className="flex flex-col gap-1 text-sm"
+                      >
+                        <span>Produk: {item.product.name}</span>
+                        <span>Quantity: {item.quantity}</span>
+                        <span>
+                          Harga Satuan:{" "}
+                          {switchCurrencyToIDR(item.product.price)}
+                        </span>
+                        <span>
+                          Total:{" "}
+                          {switchCurrencyToIDR(
+                            item.quantity * item.product.price
+                          )}
+                        </span>
+                        <div className="flex gap-2 items-center mt-2">
+                          <span>Status:</span>
+                          <Badge
+                            variant={
+                              order.status === "completed"
+                                ? "default"
+                                : "secondary"
+                            }
+                          >
+                            {isOrderPending(order.status as string) ||
+                            isOrderAwaitingShipmentNumber(
+                              order.status as string
+                            ) ? (
+                              <Clock />
+                            ) : null}
+                            {switchOrderStatus(order.status as string)}
+                          </Badge>
+                        </div>
+                        <span className="text-xs text-muted-foreground">
+                          Catatan untuk penjual: {order.merchantNote}
+                        </span>
+                        {isOrderPending(order.status as string) && (
+                          <span className="text-sm">
+                            Penting: Harap hubungi{" "}
+                            {item.merchantAdditionalInfo.phoneNumber} untuk
+                            konfirmasi pembelian
+                          </span>
+                        )}
+                        {order.shippingNumber && (
+                          <span className="text-sm">
+                            Nomor Resi: {order.shippingNumber}
+                          </span>
+                        )}
+                      </div>
+                    ))}
+                  </CardContent>
+                </Card>
+              ))
+          ) : (
+            <Card className="hidden md:flex col-span-1 lg:col-span-2">
+              <CardContent>Belum ada Transaksi terpilih</CardContent>
             </Card>
-          ))
-      ) : (
-        <Card className="hidden md:flex col-span-1 lg:col-span-2">
-          <CardContent>Belum ada Transaksi terpilih</CardContent>
-        </Card>
-      )}
-    </div>
+          )}
+        </div>
+      </main>
+    </CartPageContainer>
   );
 }
