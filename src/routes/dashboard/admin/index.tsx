3a7281da812601b5e52e67220734f8d07a73c1c0
lalatina11 <rahmadancandra@gmail.com>
2025-12-09 07:54:31 +0700

    refactor: redirect to dashboard/admin/users

--- src/routes/dashboard/admin/index.tsx
+++ src/routes/dashboard/admin/index.tsx
@@ -1,9 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
-export const Route = createFileRoute('/dashboard/admin/')({
+export const Route = createFileRoute("/dashboard/admin/")({
   component: RouteComponent,
-})
+  beforeLoad: () => {
+    throw redirect({ to: "/dashboard/admin/users" });
+  },
+});
 
 function RouteComponent() {
-  return <div>Hello "/dashboard/admin/"!</div>
+  return <div>Hello "/dashboard/admin/"!</div>;
 }
