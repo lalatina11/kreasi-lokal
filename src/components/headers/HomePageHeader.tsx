910b90603cbd90f24abdfa9718ff84be0baa1857
lalatina11 <rahmadancandra@gmail.com>
2025-12-09 07:51:22 +0700

    feat: link into dashboard/admin/ for admin role

--- src/components/headers/HomePageHeader.tsx
+++ src/components/headers/HomePageHeader.tsx
@@ -4,6 +4,7 @@ import LogoutForm from "../forms/LogoutForm";
 import { Button } from "../ui/button";
 import Logo from "./Logo";
 import { ModeToggle } from "../ModeToggle";
+import { ShoppingCart } from "lucide-react";
 
 const HomePageHeader = () => {
   const { isPending, data } = authClient.useSession();
@@ -12,6 +13,26 @@ const HomePageHeader = () => {
     <header className="flex justify-between items-center p-4 border-b">
       <Logo />
       <nav className="flex gap-2 items-center">
+        {data?.user.role === "admin" || data?.user.role === "merchant" ? (
+          <Button asChild>
+            <Link
+              to={
+                data?.user.role === "admin"
+                  ? "/dashboard/admin/users"
+                  : "/dashboard/merchant/katalog"
+              }
+            >
+              Dashboard
+            </Link>
+          </Button>
+        ) : data?.user.role === "user" ? (
+          <Button asChild>
+            <Link to="/carts">
+              <ShoppingCart />
+              Keranjang
+            </Link>
+          </Button>
+        ) : null}
         <Button asChild>
           <Link to="/products">Products</Link>
         </Button>
